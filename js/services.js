30ea1a951674f31540b763691d5e0fe18d649010
Dzmitry <40452111+Dodal@users.noreply.github.com>
2020-06-15 22:22:36 +0300

    Add files via upload

---
+++ js/services.js
@@ -0,0 +1,130 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+2. Set Header
+3. Init Menu
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/* 
+
+	1. Vars and Inits
+
+	*/
+
+	var header = $('.header');
+	var menu = $('.menu');
+	var burger = $('.hamburger');
+	var menuActive = false;
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+
+		if(window.innerWidth > 1280)
+		{
+			if(menuActive)
+			{
+				closeMenu();
+			}
+		}
+		setTimeout(function()
+		{
+			jQuery(".main_content_scroll").mCustomScrollbar("update");
+		}, 375);
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	initMenu();
+
+	/* 
+
+	2. Set Header
+
+	*/
+
+	function setHeader()
+	{
+		if($(window).scrollTop() > 91)
+		{
+			header.addClass('scrolled');
+		}
+		else
+		{
+			header.removeClass('scrolled');
+		}
+	}
+
+	/* 
+
+	3. Init Menu
+
+	*/
+
+	function initMenu()
+	{
+		if($('.hamburger').length && $('.menu').length)
+		{
+			var hamb = $('.hamburger');
+
+			hamb.on('click', function()
+			{
+				if(!menuActive)
+				{
+					openMenu();
+				}
+				else
+				{
+					closeMenu();
+				}
+			});
+		}
+	}
+
+	function openMenu()
+	{
+		menu.addClass('active');
+		menuActive = true;
+	}
+
+	function closeMenu()
+	{
+		menu.removeClass('active');
+		menuActive = false;
+	}
+
+});
+
+let contentLink = document.querySelector('.service_title a');
+let backBtn = document.createElement('button');
+		backBtn.innerHTML = 'Назад';
+		backBtn.classList.add('back__btn');
+
+	contentLink.addEventListener('click', function(e) {
+		e.preventDefault();
+		
+		let request = new XMLHttpRequest();
+		request.open('POST', 'server.html');
+		request.send();
+
+		request.addEventListener('readystatechange', function() {
+			if(request.readyState == 4) {
+				document.querySelector('.services_container').innerHTML = request.responseText;
+			}
+		});
+	});
